6aad4e30667752cdf528787831a70ab8d5f4ba6c
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-13 17:05:31 +0100

    add loop for to check emails

--- main.js
+++ main.js
@@ -8,3 +8,11 @@ stampa un messaggio appropriato sull’esito del controllo. */
 let emailsInvited = ["christopher.nolan@gmail.com", "lino.banfi@gmail.com", "gerry.scotti@gmail.com", "donald.trump@gmail.com", "shrek@gmail.com"]
 
 // 2 - dobbiamo chiedere all'utente per controllare se è invitato oppure no, quindi dichiariamo una variabile che avrà come valore il risultato prodotto (un valore booleano che sarà true se è invitato o false se non è invitato)
+
+let canAcces 
+
+// 3 - adesso controlliamo se la mail dell'utente è tra quelle degli invitati creado un ciclo for 
+
+for (let index = 0; i < emailsInvited.length; i++){
+    
+}
