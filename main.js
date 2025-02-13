268d4b9c6f6d6964d9e84fa430e173780017000a
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-13 16:54:22 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+/* Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo. */
+
+
+// 1 - la prima cosa che la traccia chiede è quella di creare una lista di email di invitati, quindi procediamo nel creare una lista tramite un un array
